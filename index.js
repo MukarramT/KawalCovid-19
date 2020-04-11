813c3f177c8908a7531ddeb7a7878585b719e673
Muhammad Ilham Rizky <milhamrizky7@gmail.com>
2020-04-12 00:58:50 +0700

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const request = require('request');
 const engine = require('ejs-locals');
+const path = require('path');
 const app = express();
+
+// let public = path.join(__dirname, '..', 'storage-covid-sumut', 'artikel');
 
 app.use(express.static('public'));
 app.use(express.json());
